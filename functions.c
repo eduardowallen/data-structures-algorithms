dbf4b7b109e1c7401a2ebd56177434eb7e7a0dad
Eduardo Wallén <Eduardo.Wallen@hotmail.com>
2020-02-14 17:08:58 +0100

    V3

--- functions.c
+++ functions.c
@@ -1,80 +1,92 @@
-#include "header.h"
+﻿#include "header.h"
 
 // Creates a new node and returns a pointer to it.
 
 Node* createNode(int key) {
-	Node* newNode = (Node*)malloc(sizeof(Node));
-	newNode->data = key;
-	newNode->prev = NULL;
-	newNode->next = NULL;
-	return newNode;
+	Node* newNode = (Node*)malloc(sizeof(Node));	// Allocate memory for the node
+	newNode->data = key;	// Set the data to key
+	newNode->prev = NULL;	// Set prev to NULL
+	newNode->next = NULL;	// Set next to NULL
+	return newNode;			// Returns node to the main function
 }
 List* createList(int key) {
-	List* newList = (List*)malloc(sizeof(List));
-	newList->empty = NULL;
-	newList->id = key;
-	return newList;
+	List* newList = (List*)malloc(sizeof(List));	// Allocate memory for the list
+	newList->empty = NULL;	// Set empty to NULL
+	newList->id = key;		// Set id to key
+	return newList;			// Return the list to the main function
 }
-void insert(List** l, Node** n) {
-	int success = 0;
+void insert(List** L, Node** N) { // Using double pointers because we want to manipulate the list and node in main function
 	printf("\nINSERTING DATA\n");
-	printf("List: %i | Node pointer: %p | Data: %i \n", (*l)->id, (*n), (*n)->data);
+	printf("List%i | Node pointer: %p | Data: %i \n", (*L)->id, (*N), (*N)->data);
 	printf("Checking if list is empty...\n");
-	if (isEmpty(l)) {
-		(*l)->node = (*n);
-		(*l)->empty = 1;
-		printf("INSERTED Node INTO LIST: %i | POINTER: %p | Data: %i \n", (*l)->id, (*l)->node, (*l)->node->data);
+	if (isEmpty(L)) {				// Check if list is empty (if true, continue)
+		(*L)->node = (*N);			// Insert the node n(that points to tempnode in main) to the list l (that origins from main)
+		(*L)->empty = 1;			// Set the list to not being empty anymore
+		printf("INSERTED Node INTO LIST: %i | POINTER: %p | Data: %i \n", (*L)->id, (*L)->node, (*L)->node->data);
 	}
 	else {
-		(*n)->prev = (*l)->node;
-		(*l)->node->next = (*n);
-		printf("INSERTED Node INTO LIST: %i | POINTER: %p | Data: %i \n", (*l)->id, (*l)->node->next, (*l)->node->next->data);
+		(*N)->prev = (*L)->node;	// Set prev for the node n(that points to tempnode in main) to point to the newest node in the list (that origins from main)
+		(*L)->node->next = (*N);	// Set next for the newest node in the list (that origins from main)
+		printf("INSERTED Node INTO LIST: %i | POINTER: %p | Data: %i \n", (*L)->id, (*L)->node->next, (*L)->node->next->data);
 	}
-	
 }
 
-int isEmpty(List** l) {
-	if ((*l)->empty == NULL) {
+int isEmpty(List** L) {
+	if ((*L)->empty == NULL) {
 		printf("LIST IS EMPTY \n");
 		return 1;
 	}
-	else
-		return 0;
+	else return 0;
 }
-Node* search(List** l, int key) {
-	int i = 1;
-	Node* tempnode = (*l)->node;
+Node* search(List** L, int key) {
+	Node* tempnode = (*L)->node;	// Allocate memory for a temporary node
 	do {
-		i++;
 		if (tempnode->data == key) {
 			printf("\nFOUND TEMPNODE HALLELUEJAH | Pointer(%p) -> Data(%d)\n", tempnode, tempnode->data);
-			return tempnode;
+			return tempnode;		// Returns tempnode to main
 		}
 		tempnode = tempnode->next;
 	} while (tempnode != NULL);
 	free(tempnode);
 }
-Node* Maximum(List** l) {
-	Node* tempnode = (*l)->node;
-	Node* bignode = NULL;
+Node* Maximum(List** L) {
+	Node* tempnode = (*L)->node; // 
+	Node* big_node = NULL;
 	int i = 0;
 	int big = 0;
 	do {
 		i++;
 		if (tempnode->data > big) {
 			big = tempnode->data;
-			bignode = tempnode;
+			big_node = tempnode;
 		}
 		tempnode = tempnode->next;
 	} while (tempnode != NULL);
-	if (bignode == NULL)
+	if (big_node == NULL)
 		printf("\nThere are no nodes in the selected list.");
-	return bignode;
+	return big_node;
 }
-void print(List** l) {
+Node* Minimum(List** L) {
+	Node* tempnode = (*L)->node; // 
+	Node* min_node = NULL;
+	int i = 0;
+	int min = 0;
+	do {
+		i++;
+		if (min < tempnode->data) {
+			min = tempnode->data;
+			min_node = tempnode;
+		}
+		tempnode = tempnode->next;
+	} while (tempnode != NULL);
+	if (min_node == NULL)
+		printf("\nThere are no nodes in the selected list.");
+	return min_node;
+}
+void print(List** L) {
 	int i = 1;
-	Node* tempnode = (*l)->node;
-	printf("\nPRINTING List: %d", (*l)->id);
+	Node* tempnode = (*L)->node;
+	printf("\nPRINTING List: %d", (*L)->id);
 	do {
 		i++;
 		printf("\n-----");
