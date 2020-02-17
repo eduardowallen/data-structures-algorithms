da14308b62f6c24aadaca3fc2e8995a6dab2fa48
Eduardo <eduardo.wallen@gmail.com>
2020-02-17 18:56:25 +0100

    V6 (mest visuella förändringar)

--- functions.c
+++ functions.c
@@ -16,29 +16,29 @@ List* createList(int key) {
 	return newList;			// Return the list to the main function
 }
 void insert(List** L, Node** N) { // Using double pointers because we want to manipulate the list and node in main function
-	printf("\nINSERTING DATA\n");
-	printf("List%i | Node pointer: %p | Data: %i \n", (*L)->id, (*N), (*N)->data);
+	printf("\nTRYING TO INSERTING DATA INTO L%d\n", (*L)->id);
+	printf("Node to insert: %p | Data: %i \n", (*N), (*N)->data);
 	printf("Checking if list is empty...\n");
 	if (isEmpty(L)) {				// Check if list is empty (if true, continue)
 		(*L)->node = (*N);			// Insert the node n(that points to tempnode in main) to the list l (that origins from main)
 		(*L)->empty = 1;			// Set the list to not being empty anymore
-		printf("INSERTED Node INTO LISTA: %i | POINTER: %p | Data: %i \n", (*L)->id, (*L)->node, (*L)->node->data);
+		printf("Inserted node as head into L%d\n", (*L)->id);
 	}
 	else {
 		Node* tempnode = (*L)->node;
-		printf("L node = %p, %d, %d", (*L)->node, (*L)->node->data, (*L)->node->next);
+		printf("L%d is not empty. Head node is %p.\n", (*L)->id, (*L)->node);
 		while (tempnode->next != NULL)
 			tempnode = tempnode->next;
-		printf("\nTempnode is now: %p\n", tempnode);
+		printf("Tempnode is now: %p\n", tempnode);
 		(*N)->prev = tempnode;	// Set prev for the node n (that points to tempnode in main) to point to the newest node in the list (that origins from main)
 		tempnode->next = (*N);	// Set next for the newest node in the list (that origins from main)
-		printf("INSERTED Node INTO LIST: %i | POINTER: %p | Data: %i \n", (*L)->id, tempnode->next, tempnode->next->data);  // Prints what list the pointer is inserted into, the node pointer and the node data.
+		printf("Inserted node into L%d\n", (*L)->id);
 	}
 }
 
 int isEmpty(List** L) {
 	if ((*L)->empty == NULL) {
-		printf("LIST IS EMPTY \n");
+		printf("L%d is empty.\n");
 		return 1;
 	}
 	else return 0;
@@ -65,7 +65,7 @@ Node* Maximum(List** L) {
 		tempnode = tempnode->next;
 	} while (tempnode != NULL);
 	if (big_node == NULL)
-		printf("\nMaximum function response: There are no nodes in the selected list.");
+		printf("Maximum function response: There are no nodes in the selected list.\n");
 	return big_node;
 }
 Node* Minimum(List** L) {
@@ -80,18 +80,18 @@ Node* Minimum(List** L) {
 		tempnode = tempnode->next;
 	} while (tempnode != NULL);
 	if (min_node == NULL)
-		printf("\nMinimum function response: There are no nodes in the selected list.");
+		printf("Minimum function response: There are no nodes in the selected list.\n");
 	return min_node;
 }
 void print(List** L) {
 	int i = 1;
 	Node* tempnode = (*L)->node;
-	printf("\nPRINTING List: %d", (*L)->id);
+	printf("PRINTING List: %d\n", (*L)->id);
 	do {
 		i++;
-		printf("\n-----");
-		printf("\n| %d |", tempnode->data);
-		printf("\n-----");
+		printf("-----\n");
+		printf("| %d |\n", tempnode->data);
+		printf("-----\n");
 		tempnode = tempnode->next;
 	} while (tempnode != NULL);
 }
--- main.c
+++ main.c
@@ -1,32 +1,96 @@
 #include "header.h"
 
 int main() {
-	int i;
-	List *List1, *List2;				// Creates List1 and List2
+	List *L1, *L2;				// Creates L1 and L2
 	// Creates lists L1 and L2
 	Node *tempnode1, *tempnode2;		// Creates a temporary node that we can use within the main function
-	List1 = createList(1);				// Allocates memory and sets its values in createList
-	List2 = createList(2);				// Allocates memory and sets its values in createList
+	L1 = createList(1);				// Allocates memory and sets its values in createList
+	L2 = createList(2);				// Allocates memory and sets its values in createList
+	// Creates the content for L1 as described in the lab exercise 2 (L1 = [3,1,5,10,8,7])
+	printf("*********************************************************************************************************\n");
+	printf("*******  Creates the content for L1 as described in the lab exercise 2 (L1 = [3, 1, 5, 10, 8, 7])  ******\n");
+	printf("*********************************************************************************************************\n");
+	tempnode1 = createNode(3);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode1
+	insert(&L1, &tempnode1);		// Inserts the created node into L1
+	tempnode1 = createNode(1);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode1
+	insert(&L1, &tempnode1);		// Inserts the created node into L1
+	tempnode1 = createNode(5);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode1
+	insert(&L1, &tempnode1);		// Inserts the created node into L1
+	tempnode1 = createNode(10);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode1
+	insert(&L1, &tempnode1);		// Inserts the created node into L1
+	tempnode1 = createNode(8);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode1
+	insert(&L1, &tempnode1);		// Inserts the created node into L1
+	tempnode1 = createNode(7);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode1
+	insert(&L1, &tempnode1);		// Inserts the created node into L1
 
+	// Exercise 2, minimum and maximum of L1
+	printf("\n\n*****************************************\n");
+	printf("******  Exercise 2, minimum of L1  ******\n");
+	printf("*****************************************\n");
+	tempnode1 = Minimum(&L1);		// Searches the list for the smallest key and returns a pointer to the node. If the list is empty the function will return a printf.
+	if (tempnode1 != NULL)
+		printf("Minimum node: data(%d), pointer(%p)\n", tempnode1->data, tempnode1);	// Prints the data in the node returned from Minimum function.
+	printf("*****************************************\n");
+
+	printf("\n\n*****************************************\n");
+	printf("******  Exercise 2, maximum of L1  ******\n");
+	printf("*****************************************\n");
+	tempnode1 = Maximum(&L1);		// Searches the list for the largest key and returns a pointer to the node. If the list is empty the function will return a printf.
+	if (tempnode1 != NULL)
+		printf("Maximum node: data(%d), pointer(%p)\n", tempnode1->data, tempnode1);	// Prints the data in the node returned from Maximum function.
+	printf("*****************************************\n");
+
+	// Creates the content for L2 as described in the lab exercise 2 (L2 = [5,2,9,6,1,2])
+	printf("\n\n\n*********************************************************************************************************\n");
+	printf("*******  Creates the content for L2 as described in the lab exercise 2  (L2 = [5, 2, 9, 6, 1, 2])  ******\n");
+	printf("*********************************************************************************************************\n");
+
+	tempnode2 = createNode(5);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode2
+	insert(&L2, &tempnode2);		// Inserts the created node into L2
+	tempnode2 = createNode(2);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode2
+	insert(&L2, &tempnode2);		// Inserts the created node into L2
+	tempnode2 = createNode(9);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode2
+	insert(&L2, &tempnode2);		// Inserts the created node into L2
+	tempnode2 = createNode(6);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode2
+	insert(&L2, &tempnode2);		// Inserts the created node into L2
+	tempnode2 = createNode(1);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode2
+	insert(&L2, &tempnode2);		// Inserts the created node into L2
+	tempnode2 = createNode(2);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode2
+	insert(&L2, &tempnode2);		// Inserts the created node into L2
+
+	// Exercise 2, minimum and maximum of L2
+	printf("\n\n*****************************************\n");
+	printf("******  Exercise 2, minimum of L2  ******\n");
+	printf("*****************************************\n");
+	tempnode2 = Minimum(&L2);		// Searches the list for the smallest key and returns a pointer to the node. If the list is empty the function will return a printf.
+	if (tempnode2 != NULL)
+		printf("Minimum node: data(%d), pointer(%p)\n", tempnode2->data, tempnode2);	// Prints the data in the node returned from Minimum function.
+	printf("*****************************************\n");
+
+	printf("\n\n*****************************************\n");
+	printf("******  Exercise 2, maximum of L2  ******\n");
+	printf("*****************************************\n");
+	tempnode2 = Maximum(&L2);		// Searches the list for the largest key and returns a pointer to the node. If the list is empty the function will return a printf.
+	if (tempnode2 != NULL)
+		printf("Maximum node: data(%d), pointer(%p)\n", tempnode2->data, tempnode2);	// Prints the data in the node returned from Maximum function.
+	printf("*****************************************\n");
+
+	/*
 	for (i = 1; i < 10; i++) {			// Inserting 10 nodes into the list, going from 1 to 10.
 		tempnode1 = createNode(i);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode1
 		tempnode2 = createNode(i);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode2
-		insert(&List1, &tempnode1);		// Inserts the created node into list 1
-		insert(&List2, &tempnode2);		// Inserts the created node into list 2
+		insert(&L1, &tempnode1);		// Inserts the created node into L1
+		insert(&L2, &tempnode2);		// Inserts the created node into L2
 	}
 	for (i = 10; i > 0; i--) {			// Inserting 10 nodes into the list, going from 10 to 1.
 		tempnode1 = createNode(i);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode1
 		tempnode2 = createNode(i);		// Creates a node (overwrites the current data in tempnode) and stores it in tempnode2
-		insert(&List1, &tempnode1);		// Inserts the created node into list 1
-		insert(&List2, &tempnode2);		// Inserts the created node into list 2
+		insert(&L1, &tempnode1);		// Inserts the created node into L1
+		insert(&L2, &tempnode2);		// Inserts the created node into L2
 	}
-	print(&List1);						// Prints list 1
-	print(&List2);						// Prints list 2
-	search(&List1, 2);					// Searches list 1 for nodes that contain the key 2 and returns a pointer to the node. Does not return anything if the list is empty.
-	tempnode1 = Maximum(&List1);		// Searches the list for the largest key and returns a pointer to the node. If the list is empty the function will return a printf.
-	tempnode2 = Minimum(&List1);		// Searches the list for the smallest key and returns a pointer to the node. If the list is empty the function will return a printf.
-	if (tempnode1 != NULL)
-		printf("\nMaximum: data(%d), pointer(%p)", tempnode1->data, tempnode1);	// Prints the data in the node returned from Maximum function.
-	if (tempnode2 != NULL)
-		printf("\nMinimum: data(%d), pointer(%p)", tempnode2->data, tempnode2);	// Prints the data in the node returned from Minimum function.
+	*/
+	//print(&L1);						// Prints L1 (for viewing purposes)
+	//print(&L2);						// Prints L2 (for viewing purposes)
+
+	//search(&L1, 2);					// Searches L1 for nodes that contain the key 2 and returns a pointer to the node. Does not return anything if the list is empty.
 }
