809a285840cac58405d19d1189a6878da044909a
Eduardo <eduardo.wallen@gmail.com>
2020-03-16 14:15:01 +0100

    Uppdaterade lite i structen till det rekommenderade sättet att namnge saker.

--- Lab 1/linkedlist.h
+++ Lab 1/linkedlist.h
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node {
+typedef struct node_t {
 	int data;			// Integer that we store the node data into
-	struct Node* next;	// Pointer to the next node
-	struct Node* prev;	// Pointer to the previous node
+	struct node_t* next;	// Pointer to the next node
+	struct node_t* prev;	// Pointer to the previous node
 } Node;
 
-typedef struct List {
-	struct Node* node;	// Pointer to the nodes inside the list
+typedef struct list_t {
+	struct node_t* node;	// Pointer to the nodes inside the list
 	int empty;			// Integer that we use to keep track if the list is empty or not
 	int id;				// Integer that we use to see what list ID we are working with
 } List;
