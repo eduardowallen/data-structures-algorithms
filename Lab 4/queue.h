8fa85cd5a53e720d4bdf6acc5cddb8b832abc674
Eduardo <eduardo.wallen@gmail.com>
2020-03-30 17:17:32 +0200

    Removing items from branch

--- Lab 4/queue.h
+++
@@ -1,8 +0,0 @@
-/* Created by Eduardo Wallén & Junior Corazza */
-#pragma once
-#include <stdio.h>
-#include <stdlib.h>
-/* Queue functions */
-void enqueue(int x);
-void dequeue();
-void display_queue();
